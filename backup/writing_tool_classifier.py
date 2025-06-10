693185b72951507b9347f25ed8eef60cd0461222
Phillip Rearick <psrearick@gmail.com>
2025-06-10 06:01:38 -0400

    wip

--- backup/writing_tool_classifier.py
+++
@@ -1,7 +0,0 @@
-# from classifier import Classifier
-
-# class WritingToolClassifier(Classifier):
-#     def __init__(self) -> None:
-#         super().__init__(['writing'])
-#         # super().__init__(['writing', 'writers', 'selfpublish', 'screenwriting'])
-#         self.topic = "software for fiction writing (using, finding, switching, complaints, support, feature requests, promotion, etc.)"
